0719bc8e70cc1c0ce2c224fc880898a52c6544bd
faithyyyy <faithfortune6@gmail.com>
2023-03-03 13:40:52 +0100

    Created an env to work with

--- src/component/Fetch.jsx
+++ src/component/Fetch.jsx
@@ -1,12 +1,2 @@
 import axios from "axios";
 export const baseUrL = "https://bayut.p.rapidapi.com";
-
-// export const fetchApi = async (url) => {
-//   const { data } = await axios.get(url, {
-//     headers: {
-//       "X-RapidAPI-Key": "a5b008000amsh780ac9c2a083a52p11ff9bjsnf7fa4ae6df97",
-//       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
-//     },
-//   });
-//   return data;
-// };
