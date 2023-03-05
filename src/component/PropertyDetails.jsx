8e9b3c35d5d0b69c928c05bbd5f814355de15d52
Faith <101983410+Faithyyyyy@users.noreply.github.com>
2023-03-05 16:59:16 +0100

    Update PropertyDetails.jsx

--- src/component/PropertyDetails.jsx
+++ src/component/PropertyDetails.jsx
@@ -90,7 +90,7 @@ function PropertyDetails() {
                       </button>
                     </Link>
                     <h3 className="font-medium uppercase mt-3 mb-3">
-                      {homeDetails?.category[1].name}
+//                       {homeDetails?.category[1].name}
                     </h3>
                     <h2 className="font-medium text-2xl lg:text-3xl italic mb-6">
                       {homeDetails?.location[2].name}
