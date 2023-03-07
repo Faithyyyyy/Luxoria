ec46087235705beef5cc4f3ac87ce5e4f1aa5c9c
faithyyyy <faithfortune6@gmail.com>
2023-03-07 17:19:44 +0100

    updated the key

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
+});
+
 export default defineConfig({
+  define: viteEnv,
   plugins: [react()],
-})
+});
