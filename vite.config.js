2e22d5017ae8a75c6ac785975fc68d99ac20a610
faithyyyy <faithfortune6@gmail.com>
2023-03-07 17:25:09 +0100

    updated the key

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-const viteEnv = {};
-Object.keys(process.env).forEach((key) => {
-  if (key.startsWith(`VITE_`)) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key];
-  }
-});
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  define: viteEnv,
   plugins: [react()],
 });
